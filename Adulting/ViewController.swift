f75bcebfd557fe5f0fdfe17f0721111d93951b8f
Tyler Phillips <thetylerjpjunk@icloud.com>
2019-07-30 19:34:36 -0400

    Initial Code Commit

---
+++ Adulting/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Adulting
+//
+//  Created by Tyler P Admin on 7/30/19.
+//  Copyright © 2019 Tyler Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
